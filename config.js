3845813da4eb7a81d276562cff629a6fdbbdff11
igirishbind-lab <igirishbind@gmail.com>
2026-01-07 17:07:12 +0530

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6fVzFz2IdjYsCt8TK_nGPwqbxrQ8hM3s",
+  authDomain: "studyos-a8e51.firebaseapp.com",
+  projectId: "studyos-a8e51",
+  storageBucket: "studyos-a8e51.firebasestorage.app",
+  messagingSenderId: "174218643372",
+  appId: "1:174218643372:web:9cbd8066f745ba4171dc09"
+};
+export default firebaseConfig;
